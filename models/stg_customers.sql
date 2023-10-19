0574549d60368e02a3c1dbc09c2e871081463532
Lucas dbt <lucasondbt202310@gmail.com>
2023-10-19 18:26:24 +0000

    adding source file to refer to raw.jaffle_shop.customers

--- models/stg_customers.sql
+++ models/stg_customers.sql
@@ -9,7 +9,8 @@ with customers as(
         id as customer_id,
         first_name,
         last_name
-    from raw.jaffle_shop.customers
+    from {{ source('jaffle_shop', 'customers') }}
+    /*from raw.jaffle_shop.customers */
 )
 
 select * from customers
